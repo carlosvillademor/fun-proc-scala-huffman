642f838730f5be9605038d6f5faee839e0889228
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2012-10-23 01:01:45 +0100

    Implementing combine function

--- src/main/scala/patmat/Huffman.scala
+++ src/main/scala/patmat/Huffman.scala
@@ -129,7 +129,11 @@ object Huffman {
    * If `trees` is a list of less than two elements, that list should be returned
    * unchanged.
    */
-  def combine(trees: List[CodeTree]): List[CodeTree] = ???
+  def combine(trees: List[CodeTree]): List[CodeTree] = trees match {
+    case Nil => trees
+    case first :: Nil => trees
+    case first::second::rest => makeCodeTree(first, second) :: rest
+  }
 
   /**
    * This function will be called in the following way:
